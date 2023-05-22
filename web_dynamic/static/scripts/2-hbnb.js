ccffbc57529be443a4d24fdb22700f55abc5173b
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-05-22 01:18:46 +0100

    Update

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -9,14 +9,17 @@ $('document').ready(function () {
     }
   });
 
-  const amenities = {};
+  let amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
       amenities[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
       delete amenities[$(this).attr('data-id')];
     }
-    $('.amenities H4').text(Object.values(amenities).join(', '));
+    if (Object.values(amenities).length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+    }
   });
-  
 });
